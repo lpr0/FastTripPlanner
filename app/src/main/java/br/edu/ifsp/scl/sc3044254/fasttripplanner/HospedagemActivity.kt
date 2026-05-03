07a68b17171699ee99de2747960a931e23b20216
lpr0 <lpr6696@gmail.com>
2026-05-03 18:41:54 -0300

    feat: enviando dados da HospedagemActivity para ResumoActivity via intents

--- app/src/main/java/br/edu/ifsp/scl/sc3044254/fasttripplanner/HospedagemActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3044254/fasttripplanner/HospedagemActivity.kt
@@ -2,18 +2,21 @@ package br.edu.ifsp.scl.sc3044254.fasttripplanner
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.scl.sc3044254.fasttripplanner.databinding.ActivityHospedagemBinding
 
 class HospedagemActivity : AppCompatActivity() {
-    //private lateinit var resumoActivityLauncher: ActivityResultLauncher<Intent>
+    private lateinit var resumoActivityLauncher: ActivityResultLauncher<Intent>
     private val binding: ActivityHospedagemBinding by lazy {
         ActivityHospedagemBinding.inflate(layoutInflater)
     }
+    private lateinit var dados: DadosViagem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +28,44 @@ class HospedagemActivity : AppCompatActivity() {
             insets
         }
 
-        val dados =  DadosViagem.lerIntent(intent)
+        dados =  DadosViagem.lerIntent(intent)
+
+        resumoActivityLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+        ) {}
+
+        binding.finish.setOnClickListener {
+            val hospedagem = tipoHosp()
+            val servicos = ArrayList<String>()
+
+            if (binding.transporte.isActivated) servicos.add("Transporte")
+            if (binding.alimentacao.isActivated) servicos.add("Alimentação")
+            if (binding.passeios.isActivated) servicos.add("Passeios")
+
+            if (hospedagem.isBlank()){
+                Toast.makeText(this,
+                    "Por favor, selecione o tipo de hospedagem", Toast.LENGTH_SHORT).show()
+            } else {
+                val resumoIntent = Intent(this, ResumoActivity::class.java)
+                DadosViagem.inserirIntent(resumoIntent, dados.destino, dados.dias, dados.orcamento,
+                    hospedagem, servicos)
+
+                resumoActivityLauncher.launch(resumoIntent)
+            }
+
+        }
 
         binding.back.setOnClickListener {
             setResult(RESULT_OK)
             finish()
         }
+    }
+
+    private fun tipoHosp (): String {
+        val id = binding.hospedagem.checkedRadioButtonId
+        if (id == R.id.conforto) return "Conforto"
+        if (id == R.id.economica) return "Econômica"
+        if (id == R.id.luxo) return "Luxo"
+        return ""
     }
 }
