7f06bccf3b14826a557b77a32f00d2bf7b3f89d2
lpr0 <lpr6696@gmail.com>
2026-05-03 16:58:08 -0300

    feat: recebendo dados da MainActivity na HospedagemActivity

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3044254/fasttripplanner/DadosViagem.kt
@@ -0,0 +1,40 @@
+package br.edu.ifsp.scl.sc3044254.fasttripplanner
+
+import android.content.Intent
+
+data class DadosViagem(
+    val destino: String,
+    val dias: Int,
+    val orcamento: Double,
+    val tipoHospedagem: String,
+    val servicos: ArrayList<String>
+) {
+    companion object util {
+        fun lerIntent (intent: Intent): DadosViagem {
+            return DadosViagem(
+                intent.getStringExtra("destino")?:"",
+                intent.getIntExtra("dias", 1),
+                intent.getDoubleExtra("orcamento", 1.00),
+                intent.getStringExtra("hospedagem")?:"",
+                intent.getStringArrayListExtra("servicos")?: ArrayList<String>()
+            )
+        }
+        fun inserirIntent(
+            intent: Intent,
+            destino: String,
+            dias: Int,
+            orcamento: Double,
+            tipoHospedagem: String?,
+            servicos: ArrayList<String>?
+        ) {
+            intent.putExtra("destino", destino)
+            intent.putExtra("dias", dias)
+            intent.putExtra("orcamento", orcamento)
+            intent.putExtra("tipoHospedagem", tipoHospedagem)
+            intent.putExtra("servicos", servicos)
+        }
+        fun inserirIntent (intent: Intent, dados: DadosViagem) {
+            inserirIntent(intent, dados.destino, dados.dias, dados.orcamento, dados.tipoHospedagem, dados.servicos)
+        }
+    }
+}
--- app/src/main/java/br/edu/ifsp/scl/sc3044254/fasttripplanner/HospedagemActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3044254/fasttripplanner/HospedagemActivity.kt
@@ -1,13 +1,32 @@
 package br.edu.ifsp.scl.sc3044254.fasttripplanner
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.sc3044254.fasttripplanner.databinding.ActivityHospedagemBinding
+import br.edu.ifsp.scl.sc3044254.fasttripplanner.databinding.ActivityMainBinding
 
 class HospedagemActivity : AppCompatActivity() {
+    private lateinit var resumoActivityLauncher: ActivityResultLauncher<Intent>
+    private val binding: ActivityHospedagemBinding by lazy {
+        ActivityHospedagemBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_hospedagem)
+        setContentView(binding.hospedagem)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val dados =  DadosViagem.lerIntent(intent)
+        binding.titulo.text = dados.destino
     }
 }
